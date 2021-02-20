7115bf8d35425495eb2809de9d519f5bbe3e1a72
Alice <wl02722691@gmail.com.tw>
2021-02-20 15:19:44 +0800

    Lecture 5: Add access control

--- 1-Memorize/Shared/EmojiMemoryGame.swift
+++ 1-Memorize/Shared/EmojiMemoryGame.swift
@@ -9,16 +9,8 @@ import SwiftUI
 import Combine
 
 class EmojiMemoryGame: ObservableObject {
-    
-    //    private(set) var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 2, cardContentFactory:{ (pairIndex: Int) -> String  in
-    //        return "🍙"
-    //    })
-    //  simplified >>>>
-    //    private(set) var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 2){ _ in  "🍙" }
-        
     @Published private var model: MemoryGame<String> = createMemoryGame()
     
-    //static func = make this a function on the type
     static func createMemoryGame() -> MemoryGame<String> {
         let emoji = ["🍜","🍘"]
         return MemoryGame<String>(numberOfPairsOfCards: 2) { pairIndex in
