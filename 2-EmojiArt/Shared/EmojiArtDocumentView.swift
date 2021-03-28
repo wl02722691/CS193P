0541b36da8666df51e90e2bb4039b7783a028ffe
Alice <wl02722691@gmail.com.tw>
2021-03-28 15:59:27 +0800

    Lecture 8: Add doubleTapToZoom

--- 2-EmojiArt/Shared/EmojiArtDocumentView.swift
+++ 2-EmojiArt/Shared/EmojiArtDocumentView.swift
@@ -33,30 +33,55 @@ struct EmojiArtDocumentView: View {
                                 Image(uiImage: self.document.backgroundImage!)
                             }
                         }
+                        .scaleEffect(self.zoomScale)
                     )
-                    .edgesIgnoringSafeArea([.horizontal, .bottom])
-                    .onDrop(of: ["public.image","public.text"], isTargeted: nil) { providers, location in
-                        var location = geometry.convert(location, from: .global)
-                        location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2)
-                        return self.drop(providers: providers, at: location)
-                    }
+                    .gesture(self.doubleTapToZoom(in: geometry.size))
                     ForEach(document.emojis) { emoji in
                         Text(emoji.text)
                             .font(self.font(for: emoji))
                             .position(self.position(for: emoji, in: geometry.size))
                     }
                 }
-                
+                .clipped()
+                .edgesIgnoringSafeArea([.horizontal, .bottom])
+                .onDrop(of: ["public.image","public.text"], isTargeted: nil) { providers, location in
+                    var location = geometry.convert(location, from: .global)
+                    location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2)
+                    location = CGPoint(x: location.x / zoomScale, y: location.y / zoomScale)
+                    return self.drop(providers: providers, at: location)
+                }
             }
         }
     }
     
+    @State private var zoomScale: CGFloat = 1.0
+    
+    private func doubleTapToZoom(in size: CGSize) -> some Gesture {
+        TapGesture(count: 2)
+            .onEnded {
+                withAnimation(.linear(duration: 4)) {
+                    self.zoomToFit(document.backgroundImage, in: size)
+                }
+            }
+    }
+    
+    private func zoomToFit(_ image: UIImage?, in size: CGSize) {
+        if let image = image, image.size.width > 0, image.size.height > 0 {
+            let hZoom = size.width / image.size.width
+            let vZoom = size.height / image.size.height
+            self.zoomScale = min(hZoom, vZoom)
+        }
+    }
+    
     private func font(for emoji: EmojiArt.Emoji) -> Font {
-        Font.system(size: emoji.fontSize)
+        Font.system(size: emoji.fontSize * zoomScale)
     }
     
     private func position(for emoji: EmojiArt.Emoji, in size: CGSize) -> CGPoint {
-        CGPoint(x: emoji.location.x + size.width/2, y: emoji.location.y + size.height/2)
+        var location = emoji.location
+        location = CGPoint(x: location.x * zoomScale, y: location.y * zoomScale)
+        location = CGPoint(x: location.x + size.width/2, y: location.y + size.height/2)
+        return location
     }
     
     private func drop(providers: [NSItemProvider], at location: CGPoint) -> Bool {
@@ -74,9 +99,3 @@ struct EmojiArtDocumentView: View {
         return found
     }
 }
-
-// 如果不想所有的 String 都 Identifiable
-// 可以用 id: \.self 的 key path 取代
-//extension String: Identifiable {
-//    public var id: String { return self }
-//}
